a2c8815ddd8517a8025949809ef37187e543519c
1337-Defender <crdsouza2004@gmail.com>
2025-05-11 15:57:00 +0400

    Added supabase initialization and switched routing to go router

--- pill_dispenser/lib/main.dart
+++ pill_dispenser/lib/main.dart
@@ -3,23 +3,26 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-// Screens
-import 'screens/home_screen.dart';
-import 'screens/auth/login_screen.dart';
-import 'screens/dashboard/dashboard_screen.dart';
-import 'screens/medications/add_medications_screen.dart';
-import 'screens/wallet/wallet_screen.dart';
-import 'screens/history/dispensing_history_screen.dart';
-import 'screens/wallet/loyalty_points_screen.dart';
-
 // Providers
 import 'providers/auth_provider.dart';
 import 'providers/dispenser_config_provider.dart';
 import 'providers/medication_provider.dart';
 import 'providers/history_provider.dart';
 import 'providers/wallet_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
   runApp(
     MultiProvider(
       providers: [
@@ -39,7 +42,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Smart Pill Dispenser',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -47,19 +50,21 @@ class MyApp extends StatelessWidget {
         textTheme: TextTheme(
           bodyMedium: TextStyle(color: Colors.black87),
         ),
+        scaffoldBackgroundColor: Colors.white,
       ),
       debugShowCheckedModeBanner: false,
       // home: const HomeScreen(),
-      initialRoute: '/login',
-      routes: {
-        '/': (ctx) => const HomeScreen(),
-        '/login': (ctx) => LoginScreen(),
-        '/dashboard': (ctx) => const DashboardScreen(),
-        '/add_medication': (ctx) => AddMedicationScreen(),
-        '/wallet': (ctx) => WalletScreen(),
-        '/history': (ctx) => DispensingHistoryScreen(),
-        '/loyalty_points': (ctx) => LoyaltyPointsScreen(),
-      },
+      // initialRoute: '/login',
+      // routes: {
+      //   '/': (ctx) => const HomeScreen(),
+      //   '/login': (ctx) => LoginScreen(),
+      //   '/dashboard': (ctx) => const DashboardScreen(),
+      //   '/add_medication': (ctx) => AddMedicationScreen(),
+      //   '/wallet': (ctx) => WalletScreen(),
+      //   '/history': (ctx) => DispensingHistoryScreen(),
+      //   '/loyalty_points': (ctx) => LoyaltyPointsScreen(),
+      // },
+      routerConfig: AppRouter.router,
     );
   }
 }
