b8eeb1338b66f08d82ddda92802deb6aff971ccd
1337-Defender <crdsouza2004@gmail.com>
2025-05-13 10:25:59 +0400

    Added a bottom navigation bar

--- pill_dispenser/lib/screens/home_screen.dart
+++ pill_dispenser/lib/screens/home_screen.dart
@@ -1,128 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:pill_dispenser/screens/auth/auth_settings_popover.dart';
+import 'package:pill_dispenser/screens/dashboard.dart';
+import 'package:popover/popover.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
 import '../../providers/auth_provider.dart';
 import '../../providers/medication_provider.dart';
 import '../../providers/wallet_provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final medicationProvider = Provider.of<MedicationProvider>(context);
-    final walletProvider = Provider.of<WalletProvider>(context);
-    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _navigateBottombar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    Dashboard(),
+    Scaffold(body: Text('Vinit')),
+    Scaffold(body: Text('Veron')),
+    Scaffold(body: Text('Shashank')),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 242, 243, 244),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: GNav(
+            onTabChange: (index) => _navigateBottombar(index),
+            gap: 8,
+            padding: const EdgeInsets.all(16),
+            tabMargin: EdgeInsets.symmetric(horizontal: 0),
+            tabBackgroundColor: Color.fromARGB(255, 239, 255, 61),
+            tabs: [
+              GButton(icon: LucideIcons.house, text: 'Home'),
+              GButton(icon: LucideIcons.settings, text: 'Configuration'),
+              GButton(icon: LucideIcons.calendarCheck, text: 'Schedule'),
+              GButton(icon: LucideIcons.store, text: 'Pharmacy'),
+              // GButton(icon: LucideIcons.wallet, text: 'Wallet')
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
-        title: const Text('Smart Pill Dispenser'),
+        // title: const Text('Smart Pill Dispenser'),
+        elevation: 0,
+        backgroundColor: Color.fromARGB(0, 0, 0, 0),
         centerTitle: true,
         actions: [
-          IconButton(
-            onPressed: () => context.push('/loyalty_points'),
-            icon: const Icon(Icons.card_giftcard),
-          ),
-          IconButton(
-            tooltip: 'Log out',
-            icon: const Icon(Icons.logout),
-            onPressed: () async {
-              await authProvider.signOut();
-              if (context.mounted) {
-                context.go('/login');
-              }
-            },
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Notifications Icon
+                Material(
+                  color: Color(0x00000000),
+                  borderRadius: BorderRadius.circular(64),
+                  child: IconButton(
+                    onPressed: () => context.push('/loyalty_points'),
+                    icon: const Icon(LucideIcons.bell, size: 24),
+                    tooltip: 'Loyalty Points',
+                  ),
+                ),
+                const SizedBox(width: 8), // Space between icons
+                // Account Icon
+                Builder(
+                  builder: (context) {
+                    return Material(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(64),
+                      child: IconButton(
+                        tooltip: 'Account',
+                        icon: const Icon(LucideIcons.userRound, size: 24),
+                        onPressed:
+                            () => showPopover(
+                              width: 100,
+                              height: 100,
+                              backgroundColor: Colors.white,
+                              context: context,
+                              bodyBuilder: (context) => AuthSettingsPopover(),
+                            ),
+                      ),
+                    );
+                  }
+                ),
+              ],
+            ),
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Wallet Info Card
-            GestureDetector(
-              onTap: () => context.push('/wallet'),
-              child: Card(
-                elevation: 4,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Wallet Balance',
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        '\$${walletProvider.balance.toStringAsFixed(3)}',
-                        style: const TextStyle(fontSize: 24, color: Colors.green),
-                      ),
-                      const SizedBox(height: 8),
-                      const Text(
-                        'Loyalty Points: 50',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // Medications List
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Your Medications',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                if (medicationProvider.medications.isNotEmpty)
-                  ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.edit),
-                    label: const Text('Edit'),
-                  ),
-              ],
-            ),
-            const SizedBox(height: 8),
-            Expanded(
-              child: ListView.builder(
-                itemCount: medicationProvider.medications.length,
-                itemBuilder: (context, index) {
-                  final medication = medicationProvider.medications[index];
-                  return Card(
-                    margin: const EdgeInsets.symmetric(vertical: 4),
-                    child: ListTile(
-                      leading: const Icon(Icons.local_pharmacy),
-                      title: Text(medication.name),
-                      subtitle: Text('Quantity: ${medication.quantity} | Schedule: ${medication.schedule}'),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          medicationProvider.removeMedication(medication.id);
-                        },
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => context.push('/add_medication'),
-        label: const Text("Add Medication"),
-        icon: const Icon(Icons.add),
-      ),
+      body: _pages[_selectedIndex],
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: () => context.push('/add_medication'),
+      //   label: const Text("Add Medication"),
+      //   icon: const Icon(Icons.add),
+      // ),
     );
   }
 }
